b2e99a8349cbc56a9f7979e1151dea2acc95c54e
Radames Ajna <radamajna@gmail.com>
2018-03-07 11:08:06 -0800

    clean up api response

--- routes/api/app.js
+++ routes/api/app.js
@@ -16,9 +16,9 @@ exports.posts = function(req, res) {
 	// Load the posts
 	var q = Task.model.find();
 
-	if(bshowAll){
+	if (bshowAll) {
 		q.find();
-	}else{
+	} else {
 		q.find({
 			$or: [{
 				"isApproved": {
@@ -31,7 +31,54 @@ exports.posts = function(req, res) {
 	q.sort('postDate');
 	q.exec(function(err, results) {
 		if (err) return res.apiError('database error - searching tasks', err);
-		posts.data = results;
+
+		results.forEach(post => {
+			var social = [];
+			if (post.hasVideo) {
+				social.push({
+					type: "video",
+					embed: post.videoEmbed,
+					url: post.videoURL
+				});
+			}
+			if (post.hasAudio) {
+				social.push({
+					type: "audio",
+					embed: post.audioEmbed,
+					url: post.audioURL
+				});
+			}
+			if (post.hasTwitter) {
+				social.push({
+					type: "twitter",
+					embed: post.twitterEmbed,
+					url: post.twitterURL
+				});
+			}
+			if (post.hasInstagram) {
+				social.push({
+					type: "instagram",
+					embed: post.instagramEmbed,
+					url: post.instagramURL
+				});
+			}
+			if (post.hasImage) {
+				social.push({
+					type: "image",
+					embed: post.imageURL,
+					url: post.imageURL,
+					source: post.imageSourceURL
+				});
+			}
+			posts.data.push({
+				slug: post.slug,
+				title: post.title,
+				description: post.decription,
+				date: post.date,
+				geo: post.location.geo,
+				social: social
+			});
+		});
 		res.apiResponse(posts.data);
 
 	}).catch(error => {
