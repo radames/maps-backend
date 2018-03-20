4df3a093fa86a7f87b951f3c33a99c7f66cb8348
Radames Ajna <radamajna@gmail.com>
2018-03-20 14:36:12 -0700

    disable root route

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
-	app.get('/', routes.views.index);
+	// app.get('/', routes.views.index); 
 	app.get('/api/twitter/:user', [keystone.middleware.api], routes.api.app.twitter);
 	app.get('/api/posts', [keystone.middleware.api, keystone.middleware.cors], routes.api.app.posts);
 	app.get('/api/posts/:pshowAll', [keystone.middleware.api, keystone.middleware.cors], routes.api.app.posts);
